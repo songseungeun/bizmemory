caa2acdc27ec5e668340a87d6bb44fef172a4732
이하은 <a01050675111@gmail.com>
2020-04-03 14:22:13 +0900

    :triangular_flag_on_post: Add render and getCardList for main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-window.onload = getCardList;
-// states
-let cardList = [];
-
-const $cardList = document.querySelector('.cardList');
-
-const $newName = document.querySelector('.newName');
-const $newCompany = document.querySelector('.newCompany');
-const $newDevision = document.querySelector('.newDevision');
-const $newPosition = document.querySelector('.newPosition');
-const $newEmail = document.querySelector('.newEmail');
-const $newMobile = document.querySelector('.newMobile');
-
-const $submitBtn = document.querySelector('.submitBtn');
-
 
 const render = () => {
   let html = '';
@@ -47,3 +32,5 @@ const getCardList = () => {
   ];
   render();
 };
+
+window.onload = getCardList;

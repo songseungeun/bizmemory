8eb270f97f43f345c172d50f9ebfd706b9820f66
kfriendship1003 <kfriendship94@gmail.com>
2020-04-03 14:01:57 +0900

    :pencil2:Fix typo in .js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@
 let cardList = [];
 
 const $cardList = document.querySelector('.cardList');
-
-const $newName = document.querySelector('.newName');
-const $newCompany = document.querySelector('.newCompany');
-const $newDevision = document.querySelector('.newDevision');
-const $newPosition = document.querySelector('.newPosition');
-const $newEmail = document.querySelector('.newEmail');
-const $newMobile = document.querySelector('.newMobile');
 const $submitBtn = document.querySelector('.submitBtn');
 
 $submitBtn.onclick = e => {

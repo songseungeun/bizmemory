0ad7e8c85b205e5b94c21e9a3e79fa68c4ed88cb
이하은 <a01050675111@gmail.com>
2020-04-03 15:18:17 +0900

    :triangular_flag_on_post: Add render and render and db for index.js

--- js/main.js
+++ js/main.js
@@ -38,6 +38,18 @@ const render = key => {
   $cardList.innerHTML = html;
 };
 
+const getCardList = () => {
+  cardList = [
+    { id: 1, name: '이하은', company: '카카오 뱅크', division: '앱 개발팀', position: '대리', email: 'daidy@naver.com', mobile: '010-5067-5111', color: 'namecard color1', favorite: true, },
+    { id: 2, name: '김우정', company: '토스', division: '인재 개발팀', position: '선입', email: 'tj123y@naver.com', mobile: '010-2344-3453', color: 'namecard color2', favorite: false, },
+    { id: 3, name: '송승은', company: '쿠팡', division: '경영지원팀', position: '과장', email: 'wj456@naver.com', mobile: '010-2535-4985', color: 'namecard color3', favorite: false, },
+    { id: 4, name: '김태진', company: '에어비앤비', division: 'UX디자인팀', position: '책임', email: 'se7890@naver.com', mobile: '010-2355-2455', color: 'namecard color4', favorite: true, },
+  ];
+  render();
+};
+
+window.onload = getCardList;
+
 const generateId = () => {
   return cardList.length ? Math.max(...cardList.map(card => card.id)) + 1 : 1;
 };
